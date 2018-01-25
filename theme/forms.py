7e30bacf0d707ac0642dbd3cd4fd5dc60912b9a4
Chris Calloway <cbc@chriscalloway.org>
2018-01-25 15:44:21 -0500

    [#74] Disable captcha

--- theme/forms.py
+++ theme/forms.py
@@ -1,4 +1,4 @@
-import requests
+# import requests
 
 from django import forms
 from django.utils.translation import ugettext, ugettext_lazy as _
@@ -81,24 +81,25 @@ class SignupForm(forms.ModelForm):
 
     password1 = forms.CharField(label="Password", widget=forms.PasswordInput())
     password2 = forms.CharField(label="Confirm Password", widget=forms.PasswordInput())
-    Captcha = forms.CharField(required=False)
-    challenge = forms.CharField()
-    response = forms.CharField()
+    # Captcha = forms.CharField(required=False)
+    # challenge = forms.CharField()
+    # response = forms.CharField()
 
     def __init__(self, request, *args, **kwargs):
         self.request = request
         super(SignupForm, self).__init__(*args, **kwargs)
 
     def verify_captcha(self):
-        params = dict(self.cleaned_data)
-        params['privatekey'] = getattr(settings, 'RECAPTCHA_PRIVATE_KEY',
-                                       "6LdNC_USAAAAADNdzytMK2-qmDCzJcgybFkw8Z5x")
-        params['remoteip'] = self.request.META['REMOTE_ADDR']
-        resp = requests.post('https://www.google.com/recaptcha/api/verify', params=params)
-        lines = resp.text.split('\n')
-        if not lines[0].startswith('false'):
-            return True
-        return False
+        # params = dict(self.cleaned_data)
+        # params['privatekey'] = getattr(settings, 'RECAPTCHA_PRIVATE_KEY',
+        #                                "6LdNC_USAAAAADNdzytMK2-qmDCzJcgybFkw8Z5x")
+        # params['remoteip'] = self.request.META['REMOTE_ADDR']
+        # resp = requests.post('https://www.google.com/recaptcha/api/verify', params=params)
+        # lines = resp.text.split('\n')
+        # if not lines[0].startswith('false'):
+        #     return True
+        # return False
+        return True
 
     def clean(self):
         if not self.verify_captcha():
