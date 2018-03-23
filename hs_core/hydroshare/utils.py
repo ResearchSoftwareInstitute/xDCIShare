f3cc3046226a16d30a55f9f62bc96fbcfd491968
Dane Summers <dsummersl@yahoo.com>
2018-03-23 09:33:11 -0400

    [#80] Fikes myhpom quota tests.
    Our zones are myhpom related, not hydroshare related. This fixes a
    number of tests.

--- hs_core/hydroshare/utils.py
+++ hs_core/hydroshare/utils.py
@@ -696,7 +696,7 @@ def validate_user_quota(user, size):
     """
     if user:
         # validate it is within quota hard limit
-        uq = user.quotas.filter(zone='hydroshare_internal').first()
+        uq = user.quotas.filter(zone='myhpom_internal').first()
         if uq:
             if not QuotaMessage.objects.exists():
                 QuotaMessage.objects.create()
