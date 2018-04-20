dfc48e3170f6fe8a91cfc26b877c80b16f679961
Dane Summers <dsummersl@yahoo.com>
2018-04-20 09:32:15 -0400

    [#144] Adds merge migrations and missing 1.14 migration.
    I'm not sure how that got missed in the last hydroshare migration, but
    it did.

---
+++ theme/migrations/0015_merge.py
@@ -0,0 +1,15 @@
+# -*- coding: utf-8 -*-
+from __future__ import unicode_literals
+
+from django.db import migrations, models
+
+
+class Migration(migrations.Migration):
+
+    dependencies = [
+        ('theme', '0014_comma_semicolon_delimiter'),
+        ('theme', '0009_auto_20180322_1459'),
+    ]
+
+    operations = [
+    ]
