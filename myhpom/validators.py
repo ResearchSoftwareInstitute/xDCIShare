9941ed444f6dea496c5dc4899dacb4c48f37fa24
Sean Harrison <sharrison@caktusgroup.com>
2018-07-03 20:35:37 -0500

    [MH-14] validators.py = custom validators, to be used both in form and model validation.

---
+++ myhpom/validators.py
@@ -0,0 +1,30 @@
+import re
+from django.core.exceptions import ValidationError
+from django.core.validators import EmailValidator, RegexValidator
+
+# First Name, Last Name: At least one alphanumeric character.
+name_validator = RegexValidator(
+    regex=r'\w',
+    flags=re.U,
+    message='Enter your name'
+)
+
+# Email: valid email address
+email_validator = EmailValidator()
+
+# Password: At least 8 chars total, 1 uppercase, lowercase, digit, special char.
+def password_validator(password):
+    errors = []
+    if len(password) < 8:
+        errors.append(u'8 characters total')
+    if re.search(r"[a-z]", password) is None:
+        errors.append(u'1 lowercase letter (a-z)')
+    if re.search(r"[A-Z]", password) is None:
+        errors.append(u'1 uppercase letter (A-Z)')
+    if re.search(r"\d", password) is None:
+        errors.append(u'1 number (0-9)')
+    if re.search(r"[!\@\#\$\%\^\*\(\)\_\+\-\=]", password) is None:
+        errors.append(u'1 special character (! @ # $ % ^ * ( ) _ + - =)')
+    if len(errors) > 0:
+        raise ValidationError(u'Enter a password with at least ' + u', '.join(errors))
+
