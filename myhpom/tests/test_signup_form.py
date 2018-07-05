455abae6ed676fc2e259c7d8babf9477620469de
Sean Harrison <sharrison@caktusgroup.com>
2018-07-05 12:45:36 -0500

    [mh-14] test signup form

---
+++ myhpom/tests/test_signup_form.py
@@ -0,0 +1,26 @@
+
+from django.test import TestCase
+from myhpom.forms.signup import SignupForm
+
+
+class SignupFormTestCase(TestCase):
+    """
+    * is_valid()==True with minimal valid data
+    """
+
+    def setUp(self):
+        self.form_data = {
+            'first_name': 'A',
+            'last_name': 'B',
+            'email': 'ab@example.com',
+            'password': 'Abbbbbbb1@',
+            'password_confirm': 'Abbbbbbb1@',
+            'state': 'NC',
+            'accept_tos': True,
+        }
+
+    def test_minimal_valid_data(self):
+        form = SignupForm(self.form_data)
+        self.assertEqual(True, form.isValid())
+        self.assertEqual({}, form.errors)
+
