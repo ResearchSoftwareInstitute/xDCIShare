842e2cbf42f96c23f61862e89ec2c0a9a3215889
Sean Harrison <sharrison@caktusgroup.com>
2018-07-06 16:06:26 -0500

    [mh-14] simplified validators.password_validator to give a single, complete message no matter what the error was.

--- myhpom/validators.py
+++ myhpom/validators.py
@@ -3,11 +3,7 @@ from django.core.exceptions import ValidationError
 from django.core.validators import EmailValidator, RegexValidator
 
 # First Name, Last Name: At least one alphanumeric character.
-name_validator = RegexValidator(
-    regex=r'\w',
-    flags=re.U,
-    message='Please enter your name'
-)
+name_validator = RegexValidator(regex=r'\w', flags=re.U, message='Please enter your name')
 
 # Email: valid email address
 email_validator = EmailValidator()
@@ -15,22 +11,23 @@ email_validator = EmailValidator()
 # Email is not already taken
 def email_not_taken_validator(email):
     from myhpom.models import User
+
     if len(User.objects.filter(email=email)) > 0:
         raise ValidationError(u'Email already in use.')
 
+
 # Password: At least 8 chars total, 1 uppercase, lowercase, digit, special char.
 def password_validator(password):
-    errors = []
-    if len(password) < 8:
-        errors.append(u'8 characters total')
-    if re.search(r"[a-z]", password) is None:
-        errors.append(u'1 lowercase letter (a-z)')
-    if re.search(r"[A-Z]", password) is None:
-        errors.append(u'1 uppercase letter (A-Z)')
-    if re.search(r"\d", password) is None:
-        errors.append(u'1 number (0-9)')
-    if re.search(r"[!\@\#\$\%\^\*\(\)\_\+\-\=]", password) is None:
-        errors.append(u'1 special character (! @ # $ % ^ * ( ) _ + - =)')
-    if len(errors) > 0:
-        raise ValidationError(u'Please enter a password with at least ' + u', '.join(errors))
-
+    if (
+        len(password) < 8
+        or re.search(r"[a-z]", password) is None
+        or re.search(r"[A-Z]", password) is None
+        or re.search(r"\d", password) is None
+        or re.search(r"[!\@\#\$\%\^\*\(\)\_\+\-\=]", password) is None
+    ):
+        raise ValidationError(
+            # non-breaking spaces for better display formatting
+            u"Please enter a password 8 characters or longer with at least 1\u00a0lowercase, "
+            + u"1\u00a0uppercase, 1\u00a0number, and 1\u00a0special character "
+            + u"\u00a0".join(u"( @ # $ % ^ * ( ) _ + - = )".split())
+        )
