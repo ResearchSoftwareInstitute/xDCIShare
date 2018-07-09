67dffe91405469d9020e8ac29fd105f4d8125658
Dane Summers <dsummersl@yahoo.com>
2018-07-09 14:18:20 -0400

    Show that objects are ordered w/o AD templates (MH-58)

--- myhpom/tests/test_state_model.py
+++ myhpom/tests/test_state_model.py
@@ -10,8 +10,9 @@ class StateModelTestCase(TestCase):
         # in alphabetical order (regardless of the template filename)
         NC = State.objects.filter(name='NC')
         SC = State.objects.filter(name='SC')
+        AK = State.objects.filter(name='AK')
         NC.update(advance_directive_template=SimpleUploadedFile('afile.txt', ''))
         SC.update(advance_directive_template=SimpleUploadedFile('zfile.txt', ''))
 
-        first_states = State.objects.order_by_ad()[:2]
-        self.assertEqual(list(first_states), [NC.first(), SC.first()])
+        first_states = State.objects.order_by_ad()[:3]
+        self.assertEqual(list(first_states), [NC.first(), SC.first(), AK.first()])
