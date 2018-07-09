725be58b8832a0b6f3f727e15872836ea8c114b9
Sean Harrison <sharrison@caktusgroup.com>
2018-07-09 11:35:09 -0500

    [mh-14] add periods to end of SignupForm errors for consistency.

--- myhpom/forms/signup.py
+++ myhpom/forms/signup.py
@@ -8,14 +8,14 @@ class SignupForm(forms.Form):
         label='First Name',
         max_length=30,
         validators=[validators.name_validator],
-        error_messages={'required': 'Please enter your first name'},
+        error_messages={'required': 'Please enter your first name.'},
     )
     middle_name = forms.CharField(label='Middle Name', max_length=30, required=False)
     last_name = forms.CharField(
         label='Last Name',
         max_length=30,
         validators=[validators.name_validator],
-        error_messages={'required': 'Please enter your last name'},
+        error_messages={'required': 'Please enter your last name.'},
     )
     email = forms.EmailField(
         label='Email Address', validators=[validators.email_not_taken_validator]
@@ -24,12 +24,12 @@ class SignupForm(forms.Form):
         label='Enter a Password',
         widget=forms.PasswordInput,
         validators=[validators.password_validator],
-        error_messages={'required': 'Please enter a password'},
+        error_messages={'required': 'Please enter a password.'},
     )
     password_confirm = forms.CharField(
         label='Confirm Password',
         widget=forms.PasswordInput,
-        error_messages={'required': 'Please enter the same password again'},
+        error_messages={'required': 'Please enter the same password again.'},
     )
     state = forms.ChoiceField(
         label='State of Residence',
@@ -40,7 +40,7 @@ class SignupForm(forms.Form):
     accept_tos = forms.BooleanField(
         label="I Agree to the Terms and Conditions of the services provided.",
         required=True,
-        error_messages={'required': 'Please agree to the Terms and Conditions'},
+        error_messages={'required': 'Please agree to the Terms and Conditions.'},
     )
 
     def clean(self):
@@ -53,7 +53,7 @@ class SignupForm(forms.Form):
             and password != password_confirm
             and ('password_confirm' not in self.errors or len(self.errors['password_confirm']) == 0)
         ):
-            self.add_error('password_confirm', "Please enter exactly the same password again")
+            self.add_error('password_confirm', "Please enter exactly the same password again.")
 
         if not self.is_valid():
             self.errors['__all__'] = [
