3d569adbe86351cc61de4736b4f980967afb91e6
Dane Summers <dsummersl@yahoo.com>
2018-07-11 16:05:25 -0400

    Fixes migrations so that they are reversable (MH-60)

--- myhpom/migrations/0008_territories.py
+++ myhpom/migrations/0008_territories.py
@@ -1,13 +1,13 @@
 # -*- coding: utf-8 -*-
-from django.db import migrations, models
+import json
+import os
 
-import os, json
 from django.db import migrations
 
 
 def load_fixture_states(apps, schema_editor):
     State = apps.get_model("myhpom", "State")
-    fixture_filename = os.path.splitext(__file__)[0]+'.json'
+    fixture_filename = os.path.splitext(__file__)[0] + '.json'
     with open(fixture_filename, 'r') as f:
         states_data = json.load(f)
     for state_data in states_data:
@@ -22,14 +22,9 @@ def unload_fixture_states(apps, schema_editor):
 class Migration(migrations.Migration):
 
     dependencies = [
-        ('myhpom', '0004_auto_20180708_0954'),
+        ('myhpom', '0007_add_is_territory'),
     ]
 
     operations = [
-        migrations.AddField(
-            model_name='state',
-            name='is_territory',
-            field=models.BooleanField(default=False),
-        ),
         migrations.RunPython(load_fixture_states, reverse_code=unload_fixture_states),
     ]
