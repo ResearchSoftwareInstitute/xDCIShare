278bd4166f0a90a21ffdd2cea9bf1c7d67a0c471
Sean Harrison <sharrison@caktusgroup.com>
2018-07-12 17:52:42 -0500

    [mh-77] date picker for /upload/requirements, uses moment.js and combodate.js

--- myhpom/views/upload_requirements.py
+++ myhpom/views/upload_requirements.py
@@ -1,9 +1,9 @@
+from datetime import datetime
 from django.shortcuts import render, redirect
 from django.contrib.auth.decorators import login_required
 from myhpom import models
 
-EARLIEST_YEAR = 1950
-
+MIN_YEAR = 1950
 
 @login_required
 def upload_requirements(request):
@@ -16,5 +16,7 @@ def upload_requirements(request):
     context = {
         'user': request.user,
         'requirements': models.StateRequirement.for_state(request.user.userdetails.state),
+        'MIN_YEAR': MIN_YEAR,
+        'MAX_YEAR': datetime.now().year,
     }
     return render(request, "myhpom/upload/requirements.html", context=context)
