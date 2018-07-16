f3b24709af001e091be7f31b028746009fba5fdb
Sean Harrison <sharrison@caktusgroup.com>
2018-07-16 16:47:26 -0500

    [mh-77] tested StateRequirementLink

--- myhpom/models/state_requirement_link.py
+++ myhpom/models/state_requirement_link.py
@@ -1,5 +1,9 @@
+import sys
 from django.db import models
+from django.core.exceptions import ValidationError
 from .state_requirement import StateRequirement
+from myhpom.validators import validate_not_blank
+
 
 class StateRequirementLink(models.Model):
     """an FAQ link for a given StateRequirement (listed under the requirement checkbox)
@@ -7,7 +11,21 @@ class StateRequirementLink(models.Model):
     * text: The text to display in the link
     * href: the href for the link
     """
+
     requirement = models.ForeignKey(StateRequirement, on_delete=models.CASCADE)
     text = models.CharField(max_length=1024)
     href = models.CharField(max_length=1024)
 
+
+def state_requirement_link_pre_save_receiver(sender, instance, **kwargs):
+    """
+    * Ensure that the StateRequirementLink fields are valid before saving:
+        * href: not blank
+        * text: not blank
+    """
+    for key in ['href', 'text']:
+        validate_not_blank(instance.__dict__.get(key))
+
+models.signals.pre_save.connect(
+    state_requirement_link_pre_save_receiver, sender=StateRequirementLink
+)
