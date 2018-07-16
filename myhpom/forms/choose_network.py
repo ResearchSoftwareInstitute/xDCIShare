35f2e6b9c1f7407dc177d17d47953f4a7cf14422
nmashton <neil.m.ashton@gmail.com>
2018-07-16 09:39:13 -0400

    [MH-101] Extra form validation to ensure exactly one variety of network is selected; consistently using "health_network" everywhere

--- myhpom/forms/choose_network.py
+++ myhpom/forms/choose_network.py
@@ -1,8 +1,18 @@
-
 from django import forms
-from myhpom import models, validators
 
 
 class ChooseNetworkForm(forms.Form):
     custom_provider = forms.CharField(max_length=1024, required=False)
     health_network = forms.IntegerField(required=False)
+
+    def clean(self):
+        cleaned_data = super(ChooseNetworkForm, self).clean()
+        custom_provider = cleaned_data.get('custom_provider')
+        health_network = cleaned_data.get('health_network')
+
+        if (not custom_provider) and (not health_network) or (custom_provider and health_network):
+            raise forms.ValidationError(
+                'Please either choose a network or enter a custom network.'
+            )
+
+        return cleaned_data
--- myhpom/views/choose_network.py
+++ myhpom/views/choose_network.py
@@ -16,29 +16,28 @@ def choose_network(request):
     """
     user_details = request.user.userdetails
     state = user_details.state
+    form = ChooseNetworkForm()
 
-    if request.method == "POST":
+    if request.POST:
         form = ChooseNetworkForm(request.POST)
-        custom_provider = (form.data.get("custom_provider") or '').strip()
-        health_network_id = form.data.get("healthcare_network")
-        health_network = None
-        if custom_provider != '':
-            # see if it actually already exists
-            networks = HealthNetwork.objects.filter(state=state, name=custom_provider.strip())
-            if len(networks) > 0:
-                network = networks[0]
-            else:
-                user_details.custom_provider = custom_provider
-                user_details.health_network = None
+        if form.is_valid():
+            custom_provider = (form.data.get("custom_provider") or '').strip()
+            health_network_id = form.data.get("health_network")
+            if custom_provider != '':
+                # see if it actually already exists
+                networks = HealthNetwork.objects.filter(state=state, name=custom_provider.strip())
+                if len(networks) > 0:
+                    user_details.health_network = networks[0]
+                else:
+                    user_details.custom_provider = custom_provider
+                    user_details.health_network = None
                 user_details.save()
-        elif health_network_id not in [None, '', 'null']:   # template sets "null"
-            health_network = HealthNetwork.objects.get(id=health_network_id)
-
-        if health_network is not None:
-            # add a reference to this network to the user's UserDetails
-            user_details.health_network = health_network
-            user_details.custom_provider = None
-            user_details.save()
+            elif health_network_id not in [None, '', 'null']:   # template sets "null"
+                try:
+                    user_details.health_network = HealthNetwork.objects.get(id=health_network_id)
+                    user_details.save()
+                except HealthNetwork.DoesNotExist:
+                    pass
 
             return redirect("myhpom:next_steps")
 
@@ -52,14 +51,15 @@ def choose_network(request):
     }
     # priority=0: 3 entries per row
     priority_0_rows = [
-        health_networks[0][(row) * 3 : ((row) * 3) + 3]
+        health_networks[0][(row) * 3:((row) * 3) + 3]
         for row in range(int(math.ceil(len(health_networks[0]) / 3.)))
     ]
-    _ = health_networks.pop(0)  # we're going to iterate only the groups with priority 1..N
+    health_networks.pop(0)  # we're going to iterate only the groups with priority 1..N
     context = {
-        'state': state,
-        'health_networks': health_networks,
+        "state": state,
+        "health_networks": health_networks,
         "priority_0_rows": priority_0_rows,
         "PRIORITY": PRIORITY,
+        "form": form,
     }
     return render(request, 'myhpom/accounts/choose_network.html', context=context)
