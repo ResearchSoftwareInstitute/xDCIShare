555ef59b82ae2755ce0e402b08e65ae5652600eb
Sean Harrison <sharrison@caktusgroup.com>
2018-07-17 12:06:40 -0500

    [mh-11] /download/template displays the AD template for the user's state, else 404

--- myhpom/urls.py
+++ myhpom/urls.py
@@ -9,6 +9,7 @@ urlpatterns = [
     url(r'^upload/current_ad/?$', myhpom_views.upload_current_ad, name='upload_current_ad'),
     url(r'^upload/requirements/?$', myhpom_views.upload_requirements, name='upload_requirements'),
     url(r'^upload/sharing/?$', myhpom_views.upload_sharing, name='upload_sharing'),
+    url(r'^download/template/?$', myhpom_views.download_template, name='download_template'),
     url(r'^logout/?$', myhpom_views.logout, name='logout'),
     url(r'^accounts/signup/?$', myhpom_views.signup, name='signup'),
     url(r'^accounts/next-steps/?$', myhpom_views.next_steps, name='next_steps'),
