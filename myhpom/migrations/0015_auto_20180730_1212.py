2fd035849d1addeecdbe56e3eb9fc81b7d451af0
Sean Harrison <sharrison@caktusgroup.com>
2018-07-30 15:14:05 -0500

    [mh-12] additional fields in UserDetails model as needed for Edit Profile

---
+++ myhpom/migrations/0015_auto_20180730_1212.py
@@ -0,0 +1,60 @@
+# -*- coding: utf-8 -*-
+from __future__ import unicode_literals
+
+from django.db import migrations, models
+import django.db.models.deletion
+
+
+class Migration(migrations.Migration):
+
+    dependencies = [
+        ('myhpom', '0014_advancedirective_original_filename'),
+    ]
+
+    operations = [
+        migrations.AddField(
+            model_name='userdetails',
+            name='birthdate',
+            field=models.DateField(help_text="The user's date of birth.", null=True, blank=True),
+        ),
+        migrations.AddField(
+            model_name='userdetails',
+            name='gender',
+            field=models.CharField(blank=True, max_length=32, null=True, help_text="The user's gender self-identification.", choices=[(b'Male', b'Male'), (b'Female', b'Female'), (b'Non-Binary / Fluid', b'Non-Binary / Fluid'), (b'Non-Gender-Conforming', b'Non-Gender-Conforming')]),
+        ),
+        migrations.AddField(
+            model_name='userdetails',
+            name='phone',
+            field=models.CharField(help_text='Phone number at which the user can be contacted.', max_length=32, null=True, blank=True),
+        ),
+        migrations.AddField(
+            model_name='userdetails',
+            name='zip_code',
+            field=models.CharField(help_text="The zip code for the user's health care address.", max_length=10, null=True, blank=True),
+        ),
+        migrations.AlterField(
+            model_name='userdetails',
+            name='accept_tos',
+            field=models.NullBooleanField(help_text='Whether the user has accepted the Terms of Service.'),
+        ),
+        migrations.AlterField(
+            model_name='userdetails',
+            name='custom_provider',
+            field=models.CharField(help_text="The name of the user's health care provider, if not in our database.", max_length=1024, null=True, blank=True),
+        ),
+        migrations.AlterField(
+            model_name='userdetails',
+            name='health_network',
+            field=models.ForeignKey(on_delete=django.db.models.deletion.SET_NULL, to='myhpom.HealthNetwork', help_text='The health network that the user belongs to.', null=True),
+        ),
+        migrations.AlterField(
+            model_name='userdetails',
+            name='middle_name',
+            field=models.CharField(help_text="The user's middle name, if any.", max_length=30, null=True, blank=True),
+        ),
+        migrations.AlterField(
+            model_name='userdetails',
+            name='state',
+            field=models.ForeignKey(on_delete=django.db.models.deletion.SET_NULL, blank=True, to='myhpom.State', help_text='The state in which the user receives health care.', null=True),
+        ),
+    ]
--- myhpom/models/user_details.py
+++ myhpom/models/user_details.py
@@ -4,6 +4,9 @@ from .state import State
 from .health_network import HealthNetwork
 
 
+GENDER_CHOICES = [(k, k) for k in ['Male', 'Female', 'Non-Binary / Fluid', 'Non-Gender-Conforming']]
+
+
 class UserDetails(models.Model):
     """Added user details to that are included when the user signs up.
     * user = the User object that these details are associated with
@@ -14,8 +17,39 @@ class UserDetails(models.Model):
     """
 
     user = models.OneToOneField(User)
-    state = models.ForeignKey(State, on_delete=models.SET_NULL, null=True, blank=True)
-    middle_name = models.CharField(max_length=30, null=True, blank=True)
-    accept_tos = models.NullBooleanField()
-    health_network = models.ForeignKey(HealthNetwork, on_delete=models.SET_NULL, null=True)
-    custom_provider = models.CharField(max_length=1024, null=True, blank=True)
+    accept_tos = models.NullBooleanField(
+        help_text="Whether the user has accepted the Terms of Service."
+    )
+    state = models.ForeignKey(
+        State, on_delete=models.SET_NULL, null=True, blank=True,
+        help_text="The state in which the user receives health care.",
+    )
+    middle_name = models.CharField(
+        max_length=30, null=True, blank=True, help_text="The user's middle name, if any."
+    )
+    health_network = models.ForeignKey(
+        HealthNetwork, on_delete=models.SET_NULL, null=True,
+        help_text="The health network that the user belongs to."
+    )
+    custom_provider = models.CharField(
+        max_length=1024, null=True, blank=True,
+        help_text="The name of the user's health care provider, if not in our database."
+    )
+    # zip + 4 = 10 characters
+    zip_code = models.CharField(
+        max_length=10, null=True, blank=True,
+        help_text="The zip code for the user's health care address."
+    )
+    # free form phone number, long enough to include ext. etc.
+    phone = models.CharField(
+        max_length=32, null=True, blank=True,
+        help_text="Phone number at which the user can be contacted."
+    )
+    birthdate = models.DateField(
+        null=True, blank=True,
+        help_text="The user's date of birth."
+    )
+    # gender according to the GENDER_CHOICES above
+    gender = models.CharField(max_length=32, null=True, blank=True, choices=GENDER_CHOICES, 
+        help_text="The user's gender self-identification."
+    )
