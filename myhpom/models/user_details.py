7cae6593a30d0a69a0e6bad3000f4905d2f4fca8
Sean Harrison <sharrison@caktusgroup.com>
2018-08-02 10:45:22 -0500

    [mh-12] UserDetails tests

--- myhpom/models/user_details.py
+++ myhpom/models/user_details.py
@@ -21,42 +21,69 @@ class UserDetails(models.Model):
         help_text="Whether the user has accepted the Terms of Service."
     )
     state = models.ForeignKey(
-        State, on_delete=models.SET_NULL, null=True, blank=True,
+        State,
+        on_delete=models.SET_NULL,
+        null=True,
+        blank=True,
         help_text="The state in which the user receives health care.",
     )
     middle_name = models.CharField(
         max_length=30, null=True, blank=True, help_text="The user's middle name, if any."
     )
     health_network = models.ForeignKey(
-        HealthNetwork, on_delete=models.SET_NULL, null=True,
-        help_text="The health network that the user belongs to."
+        HealthNetwork,
+        on_delete=models.SET_NULL,
+        null=True,
+        help_text="The health network that the user belongs to.",
     )
     custom_provider = models.CharField(
-        max_length=1024, null=True, blank=True,
-        help_text="The name of the user's health care provider, if not in our database."
+        max_length=1024,
+        null=True,
+        blank=True,
+        help_text="The name of the user's health care provider, if not in our database.",
     )
-    health_network_updated = models.DateTimeField(auto_now_add=True, null=True,
-        help_text="When the user's health network selection was last updated."
+    health_network_updated = models.DateTimeField(
+        auto_now_add=True,
+        null=True,
+        help_text="When the user's health network selection was last updated.",
+    )
+    birthdate = models.DateField(null=True, blank=True, help_text="The user's date of birth.")
+    # gender according to the GENDER_CHOICES above
+    gender = models.CharField(
+        max_length=32,
+        null=True,
+        blank=True,
+        choices=GENDER_CHOICES,
+        help_text="The user's gender self-identification.",
     )
     # zip + 4 = 10 characters
     zip_code = models.CharField(
-        max_length=10, null=True, blank=True,
-        help_text="The zip code for the user's health care address."
+        max_length=10,
+        null=True,
+        blank=True,
+        help_text="The zip code for the user's health care address.",
     )
     # free form phone number, long enough to include ext. etc.
     phone = models.CharField(
-        max_length=32, null=True, blank=True,
-        help_text="Phone number at which the user can be contacted."
-    )
-    birthdate = models.DateField(
-        null=True, blank=True,
-        help_text="The user's date of birth."
-    )
-    # gender according to the GENDER_CHOICES above
-    gender = models.CharField(max_length=32, null=True, blank=True, choices=GENDER_CHOICES, 
-        help_text="The user's gender self-identification."
+        max_length=32,
+        null=True,
+        blank=True,
+        help_text="Phone number at which the user can be contacted.",
     )
     # organ donor status
-    is_organ_donor = models.BooleanField(default=False, 
-        help_text="Whether the user is registered as an organ donor."
+    is_organ_donor = models.BooleanField(
+        default=False, help_text="Whether the user is registered as an organ donor."
     )
+
+
+def user_details_pre_save_receiver(sender, instance, **kwargs):
+    """
+    * Ensure that the User fields are valid before saving:
+        * gender
+    """
+    choices = [choice[0] for choice in GENDER_CHOICES]
+    if instance.gender is not None and instance.gender not in choices:
+        raise ValueError('Gender must be one of %s' % ', '.join(choices))
+
+
+models.signals.pre_save.connect(user_details_pre_save_receiver, sender=UserDetails)
--- myhpom/tests/test_user_details_model.py
+++ myhpom/tests/test_user_details_model.py
@@ -1,16 +1,71 @@
 from django.test import TestCase
 from django.core.exceptions import ValidationError
-from myhpom.models.user import User
-from myhpom.models.user_details import UserDetails
+from django.db import DataError
+from myhpom.tests.factories import UserFactory
+from myhpom import models
+from myhpom.models.user_details import GENDER_CHOICES
+from datetime import date
 
 
 class UserDetailsModelTestCase(TestCase):
     """
-    * 
+    * create with no parameters: is_organ_donor = False, health_network_updated = now
+    * state must be an existing state
+    * health_network must be an existing health network
+    * custom_provider must be no longer than 1024 characters
+    * gender must be one of GENDER_CHOICES
+    * birthdate must be a date
+    * zip_code must be no longer than 10 characters
+    * phone must be no longer than 32 characters
     """
 
     def setUp(self):
-        user_data = {'first_name': 'A', 'last_name': 'B', 'email': 'Ab@example.com'}
-        self.user = User.objects.create(**user_data)
-        user_details = {'user': self.user}
-        UserDetails.objects.create(**user_details)
+        self.userdetails = UserFactory().userdetails
+
+    def test__create_no_params(self):
+        self.assertFalse(self.userdetails.is_organ_donor)
+        self.assertIsInstance(self.userdetails.health_network_updated, date)
+
+    def test__state_not_existing(self):
+        self.userdetails.state = models.State.objects.first()  # ok
+        self.userdetails.save()
+        self.userdetails.state = models.State(name='UU')  # UU is not a state
+        self.assertRaises(ValueError, self.userdetails.save)
+
+    def test__health_network_not_existing(self):
+        self.userdetails.health_network = models.HealthNetwork.objects.first()  # ok
+        self.userdetails.save()
+        self.userdetails.health_network = models.HealthNetwork(
+            state=models.State.objects.first(), name='', priority=0
+        )
+        self.assertRaises(ValueError, self.userdetails.save)
+
+    def test__custom_provider(self):
+        self.userdetails.custom_provider = "A" * 1024  # just right
+        self.userdetails.save()
+        self.userdetails.custom_provider = "A" * 1025  # too long
+        self.assertRaises(DataError, self.userdetails.save)
+
+    def test__gender(self):
+        self.userdetails.gender = GENDER_CHOICES[0][0]  # ok
+        self.userdetails.save()
+        self.userdetails.gender = "NON_CONFORMING_TO_GENDER_CHOICES"
+        self.assertRaises(ValueError, self.userdetails.save)
+
+    def test__birthdate(self):
+        self.userdetails.birthdate = date.today()  # ok
+        self.userdetails.save()
+        self.userdetails.birthdate = 'today'  # not ok
+        self.assertRaises(ValidationError, self.userdetails.save)
+
+    def test__zip_code(self):
+        self.userdetails.zip_code = '12345-6789'  # zip+4 ok
+        self.userdetails.save()
+        self.userdetails.zip_code = '12345-67890'  # zip+5 too long
+        self.assertRaises(DataError, self.userdetails.save)
+
+    def test__phone(self):
+        self.userdetails.phone = '8' * 32  # ok
+        self.userdetails.save()
+        self.userdetails.phone = '8' * 33  # too long
+        self.assertRaises(DataError, self.userdetails.save)
