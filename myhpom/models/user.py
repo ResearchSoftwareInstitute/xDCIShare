662930f3401fa0f85a1b5cc5efbbe70a9b6c64bc
Sean Harrison <sharrison@caktusgroup.com>
2018-08-02 21:18:09 -0500

    [mh-12 + mh-152] username must always = email.

--- myhpom/models/user.py
+++ myhpom/models/user.py
@@ -11,13 +11,12 @@ def user_pre_save_receiver(sender, instance, **kwargs):
         * first_name
         * last_name
         * email
-    * If the username hasn't been set, set it to the email
+    * Set the username to the email
     """
     validators.name_validator(instance.first_name)
     validators.name_validator(instance.last_name)
     validators.email_validator(instance.email)
-    if instance.username in [None, '']:
-        instance.username = instance.email
+    instance.username = instance.email
 
 
 models.signals.pre_save.connect(user_pre_save_receiver, sender=User)
