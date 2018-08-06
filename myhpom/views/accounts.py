e398ba5a352cae7f095506cd6754f91a6f1df157
Sean Harrison <sharrison@caktusgroup.com>
2018-08-06 17:55:06 -0500

    [mh-10] ChooseNetworkForm is the only one we need.

--- myhpom/views/accounts.py
+++ myhpom/views/accounts.py
@@ -2,7 +2,7 @@ from django.shortcuts import render, redirect
 from django.views.decorators.http import require_GET
 from django.contrib.auth.decorators import login_required
 
-from myhpom.forms.choose_network import ChooseNetworkNoADTemplateForm
+from myhpom.forms.choose_network import ChooseNetworkForm
 
 
 @login_required
@@ -15,13 +15,10 @@ def next_steps(request):
         }
         return render(request, 'myhpom/accounts/next_steps.html', context)
 
-    form = ChooseNetworkNoADTemplateForm(instance=request.user.userdetails)
+    form = ChooseNetworkForm(instance=request.user.userdetails)
 
     if request.POST:
-        form = ChooseNetworkNoADTemplateForm(
-            request.POST,
-            instance=request.user.userdetails,
-        )
+        form = ChooseNetworkForm(request.POST, instance=request.user.userdetails)
         if form.is_valid():
             form.save()
             return redirect("myhpom:dashboard")
