8d877b743faf9ba222ef0b6d63a1179536a17073
Sean Harrison <sharrison@caktusgroup.com>
2018-08-07 11:40:38 -0500

    [mh-10] distinguish between signup (choose_network) and update_network via different urls. update_network url is linked from the dashboard, choose_network is used during signup.

--- myhpom/views/choose_network.py
+++ myhpom/views/choose_network.py
@@ -1,12 +1,14 @@
 from django.shortcuts import render, redirect
+from django.core.urlresolvers import reverse
 from django.contrib.auth.decorators import login_required
 from myhpom.models.health_network import HealthNetwork, PRIORITY
 from myhpom.forms.choose_network import ChooseNetworkForm
 import math
+from datetime import datetime
 
 
 @login_required
-def choose_network(request):
+def choose_network(request, is_update=False):
     """
     Allow the logged in user to choose their health network.
     If the user is in an unsupported state, redirect to next_steps. Otherwise,
@@ -18,18 +20,20 @@ def choose_network(request):
     form = ChooseNetworkForm(instance=request.user.userdetails)
 
     if request.POST:
-        form = ChooseNetworkForm(
-            request.POST,
-            instance=request.user.userdetails,
-        )
+        form = ChooseNetworkForm(request.POST, instance=request.user.userdetails)
         if form.is_valid():
             form.save()
-
-            return redirect("myhpom:next_steps")
+            if is_update:
+                return redirect("myhpom:dashboard")
+            else:
+                return redirect("myhpom:next_steps")
 
     # request.method == 'GET'
     if not user_details.state.healthnetwork_set.exists():
-        return redirect("myhpom:next_steps")
+        if is_update:
+            pass    # use the next_steps_no_ad form
+        else:
+            return redirect(reverse("myhpom:next_steps"))
 
     state_networks = HealthNetwork.objects.filter(state=state)
     health_networks = {
@@ -37,7 +41,7 @@ def choose_network(request):
     }
     # priority=0: 3 entries per row
     priority_0_rows = [
-        health_networks[0][(row) * 3:((row) * 3) + 3]
+        health_networks[0][(row) * 3 : ((row) * 3) + 3]
         for row in range(int(math.ceil(len(health_networks[0]) / 3.)))
     ]
     health_networks.pop(0)  # we're going to iterate only the groups with priority 1..N
@@ -47,5 +51,6 @@ def choose_network(request):
         "priority_0_rows": priority_0_rows,
         "PRIORITY": PRIORITY,
         "form": form,
+        "is_update": is_update,
     }
     return render(request, 'myhpom/accounts/choose_network.html', context=context)
