8cea6d6c4ef02bf27cb7044dd32055278746a7c4
Sean Harrison <sharrison@caktusgroup.com>
2018-08-09 16:20:27 -0500

    [mh-143] omit needless words

--- myhpom/migrations/0016_scribbles_data.py
+++ myhpom/migrations/0016_scribbles_data.py
@@ -1,7 +1,7 @@
 # -*- coding: utf-8 -*-
 from __future__ import unicode_literals
 
-from django.db import migrations, models
+from django.db import migrations
 from django.core.urlresolvers import reverse
 from scribbler.models import Scribble
 
