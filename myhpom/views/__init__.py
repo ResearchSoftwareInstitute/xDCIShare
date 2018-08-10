89d62dab2f7c92fc447bf8b26c561f793a0d9b74
Dane Summers <dsummersl@yahoo.com>
2018-08-10 16:54:27 -0400

    Adds FAQ entries and bootstrap styling (MH-25)

--- myhpom/views/__init__.py
+++ myhpom/views/__init__.py
@@ -1,3 +1,7 @@
+import os
+import yaml
+
+from django.conf import settings
 from django.shortcuts import render, redirect
 from django.contrib.auth.decorators import login_required
 from django.views.decorators.http import require_GET
@@ -13,12 +17,23 @@ from myhpom.views.upload import (upload_current_ad, upload_index, upload_require
 from myhpom.views.signup import signup
 from myhpom.views.profile import (edit_profile, view_profile)
 
+FAQS = yaml.load(open(os.path.join(settings.PROJECT_ROOT, '../myhpom/static/myhpom/data/faq.yaml'), 'r'))
+
+
 @require_GET
 def home(request):
     if request.user.is_authenticated():
         return redirect(reverse('myhpom:dashboard'))
 
     return render(request, 'myhpom/home.html')
+
+
+@require_GET
+def faq(request):
+    context = {
+        'faqs': FAQS
+    }
+    return render(request, 'myhpom/faq.html', context)
 
 
 @require_GET
