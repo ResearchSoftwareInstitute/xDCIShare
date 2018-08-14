29af51dfda3c6d9cb35d2854229d944c8e524f02
Sean Harrison <sharrison@caktusgroup.com>
2018-08-14 16:30:35 -0500

    [mh-72] Document.thumbnail model and methods. * Document.thumbnail is a nullable field that stores the thumbnail image for the document. * Document.save_thumbnail() renders a thumbnail image from the current document PDF first page. * Document.render_images() is a helper function that uses ghostscript (via bgs.GS) to render the pages of a PDF to images; the save_thumbnail() method only renders the first page but the render_images() method is useful for later rendering all pages via Celery if we so choose. * remove_documents_on_delete() also removes any thumbnail file associated with the document.

--- myhpom/models/document.py
+++ myhpom/models/document.py
@@ -1,8 +1,10 @@
-import os
+import os, tempfile
 
 from .user import User
 from django.db import models
+from django.core.files.base import ContentFile
 from myhpom.validators import validate_date_in_past
+from bgs import GS
 
 
 class AdvanceDirective(models.Model):
@@ -14,27 +16,65 @@ class AdvanceDirective(models.Model):
         upload_to='myhpom/advance_directives',
         help_text='The document representing this user\'s Advance Directive.',
     )
-    original_filename = models.CharField(
-        max_length=512,
-        blank=True,
-    )
+    original_filename = models.CharField(max_length=512, blank=True)
     valid_date = models.DateField(
-        help_text='Date that this document is legally valid.',
-        validators=[validate_date_in_past],
+        help_text='Date that this document is legally valid.', validators=[validate_date_in_past]
     )
     share_with_ehs = models.BooleanField(
         help_text=('True when user this document can be shared with the user\'s healthcare system.')
+    )
+    thumbnail = models.FileField(
+        null=True,
+        upload_to='myhpom/advance_directives',
+        help_text='The first-page thumbnail image of the user\'s Advance Directive.',
     )
 
     @property
     def filename(self):
         return self.original_filename or os.path.basename(self.document.name)
 
+    def save_thumbnail(self, save=True, res=150, **gsargs):
+        self.render_images(save=save, save_thumbnail=True, allpages=False, res=res, **gsargs)
+
+    def render_images(self, save=True, save_thumbnail=True, allpages=True, res=150, **gsargs):
+        """create one or more images of pages of the current document. 
+        Requires that the AD has a document in storage
+        Returns a list of (temporary) filenames (deleted after the AD object goes out of scope).
+        * save=True: if true, saves (persists) the underlying model
+        * save_thumbnail=True: if true, saves the first image as the AD.thumbnail
+        * allpages=False: if true, creates all pages. if False, creates only the first page.
+            (if all you want is a thumbnail, creating only the first page is MUCH faster)
+        * res=150: the resolution of the output images based on the pdf page size
+        * **gsargs: The GS.render() method takes several other arguments that you can specify:
+            * outfn = specify the output filename. If multiple pages, a numerical counter is added.
+            * device = the device to use to create the output; implies the file extension
+            * alpha = the number of bits to use for the alpha value.
+            * quality = the jpeg quality: 100 = highest.
+        """
+        gs = GS()  # ghostscript interpreter
+        # first write the document to a temporary file in the filesystem
+        tempdir = tempfile.mkdtemp()
+        pdf_filename = os.path.join(tempdir, self.filename)
+        fd = self.document.storage.open(self.document.name, 'rb').read()
+        with open(pdf_filename, 'wb') as f:
+            f.write(fd)
+        # then use ghostscript to render it
+        image_filenames = gs.render(pdf_filename, res=res, allpages=allpages, **gsargs)
+        # and save a thumbnail
+        if save_thumbnail is True and len(image_filenames or []) > 0:
+            thumbnail_filename = image_filenames[0]
+            fd = open(thumbnail_filename, 'rb').read()
+            # the save name of the thumbnail is the document name + ext
+            name = self.document.name + os.path.splitext(thumbnail_filename)[-1]
+            self.thumbnail.save(name, ContentFile(fd), save=save)
+        return image_filenames
+
 
 def remove_documents_on_delete(sender, instance, using, **kwargs):
-    if not instance.document:
-        return
-    instance.document.storage.delete(instance.document.name)
+    if instance.thumbnail:
+        instance.thumbnail.storage.delete(instance.thumbnail.name)
+    if instance.document:
+        instance.document.storage.delete(instance.document.name)
 
 
 models.signals.post_delete.connect(remove_documents_on_delete, sender=AdvanceDirective)
