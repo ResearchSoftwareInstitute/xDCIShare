5b9d482c12cf8e5e7c4ec363b077eaffbe658a05
Sean Harrison <sharrison@caktusgroup.com>
2018-08-17 15:35:26 -0500

    [mh-209] include thumbnail generation in the validation stage rather than the save stage. This means that the UploadRequirementsForm needs a thumbnail field so we can include it in the validation; however, the validation message into the 'document' errors – because if a thumbnail couldn't be created, it's an error with the document, not with the thumbnail. We thus don't need the customized form save() method.

--- myhpom/forms/upload_requirements.py
+++ myhpom/forms/upload_requirements.py
@@ -1,5 +1,6 @@
 from django import forms
 from django.conf import settings
+from django.core.files.uploadedfile import SimpleUploadedFile
 from myhpom.models import AdvanceDirective
 
 import os
@@ -11,24 +12,24 @@ class UploadRequirementsForm(forms.ModelForm):
 
     class Meta:
         model = AdvanceDirective
-        fields = ['valid_date', 'document', 'original_filename']
+        fields = ['valid_date', 'document', 'thumbnail', 'original_filename']
 
     def clean_document(self):
-        document = self.cleaned_data['document']
+            document = self.cleaned_data['document']
         if document is None:
             return None
 
-        errors = []
-        if not document.name.lower().endswith('.pdf'):
-            errors.append(forms.ValidationError('File can only be PDF format'))
-        if document.size > settings.MAX_AD_SIZE:
+            errors = []
+            if not document.name.lower().endswith('.pdf'):
+                errors.append(forms.ValidationError('File can only be PDF format'))
+            if document.size > settings.MAX_AD_SIZE:
             errors.append(forms.ValidationError(
                 'File must be smaller than %d megabytes' % (settings.MAX_AD_SIZE / 1024 / 1024)))
 
-        if len(errors) > 0:
-            raise forms.ValidationError(errors)
+            if len(errors) > 0:
+                raise forms.ValidationError(errors)
 
-        return document
+            return document
 
     def clean(self):
         data = self.cleaned_data
@@ -37,12 +38,18 @@ class UploadRequirementsForm(forms.ModelForm):
             name, extension = os.path.splitext(data['document'].name)
             data['document'].name = "%s-%s%s" % (name, str(uuid.uuid4())[:6], extension)
 
+            # (try to) generate a thumbnail of the document
+            try:
+                thumbnail_name = os.path.splitext(data['document'].name)[0] + '.jpg'
+                thumbnail_data = AdvanceDirective(document=data['document']).render_thumbnail_data()
+                data['thumbnail'] = SimpleUploadedFile(thumbnail_name, thumbnail_data)
+            except:
+                data['document'] = ''
+                self.add_error(
+                    'document',
+                    'The uploaded PDF file was corrupt or invalid and could not be saved.',
+                )
         return data
-
-    def save(self, commit=True):
-        saved = super(forms.ModelForm, self).save(commit=commit)
-        self.instance.save_thumbnail(save=commit)
-        return saved
 
 
 class SharingForm(forms.ModelForm):
