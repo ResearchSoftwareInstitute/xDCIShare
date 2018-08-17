ae7c9d1f67f7069b9be48755ab59ef1d90d379d0
Sean Harrison <sharrison@caktusgroup.com>
2018-08-17 15:31:36 -0500

    [mh-209] rework the creating of thumbnails and pdf images in general: instead of saving the thumbnail here, just use these methods to render the images from the AdvanceDirective object. — In fact, we're not providing a Model interface to save thumbnails at all; instead, we're relying on the form to handle that. At the model level, we can always give the model a thumbnail file and save it.

--- myhpom/tests/test_advance_directive_model.py
+++ myhpom/tests/test_advance_directive_model.py
@@ -10,8 +10,8 @@ from myhpom.tests.factories import UserFactory
 
 PDF_FILENAME = os.path.join(os.path.dirname(__file__), 'fixtures', 'afile.pdf')
 
-class RemoveDocumentsOnDeleteTest(TestCase):
 
+class RemoveDocumentsOnDeleteTest(TestCase):
     def test_a_file(self):
         # When an AD is deleted its corresponding file and thumbnail should be deleted from
         # the system. (this test requires an actual PDF)
@@ -20,8 +20,11 @@ class RemoveDocumentsOnDeleteTest(TestCase):
             document = SimpleUploadedFile(os.path.basename(PDF_FILENAME), f.read())
         directive = AdvanceDirective(
             user=user, share_with_ehs=False, document=document, valid_date=now())
+        directive.thumbnail = SimpleUploadedFile(
+            os.path.splitext(os.path.basename(PDF_FILENAME))[0] + '.jpg',
+            directive.render_thumbnail_data(),
+        )
         directive.save()
-        directive.save_thumbnail()
         self.assertTrue(directive.document.storage.exists(directive.document.name))
         self.assertTrue(directive.thumbnail.storage.exists(directive.thumbnail.name))
 
