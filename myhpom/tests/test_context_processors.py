6438c84f4cb68054fa202b87a36a379d86bf6630
Sean Harrison <sharrison@caktusgroup.com>
2018-08-30 09:36:00 -0500

    [mh-246] make tests pass even when settings.CONTACT_EMAIL is not None

--- myhpom/tests/test_context_processors.py
+++ myhpom/tests/test_context_processors.py
@@ -1,5 +1,6 @@
 from django.core.urlresolvers import reverse
 from django.test import TestCase
+from django.conf import settings
 from myhpom.tests.factories import UserFactory
 from myhpom.urls import static_views
 
@@ -8,8 +9,7 @@ class ContactEmailProcessorTestCase(TestCase):
     def test_email_is_available_on_home(self):
         for example_email in ('foo@example.com', 'bar@example.com', ):
             response = self.client.get(reverse('myhpom:home'))
-            self.assertIn('contact_email', response.context)
-            self.assertIsNone(response.context['contact_email'])
+            self.assertEqual(response.context['contact_email'], settings.CONTACT_EMAIL)
             with self.settings(CONTACT_EMAIL=example_email):
                 response = self.client.get(reverse('myhpom:home'))
                 self.assertIn('contact_email', response.context)
@@ -22,8 +22,7 @@ class ContactEmailProcessorTestCase(TestCase):
         self.assertTrue(self.client.login(username=user.username, password='password'))
         for example_email in ('foo@example.com', 'bar@example.com', ):
             response = self.client.get(reverse('myhpom:dashboard'))
-            self.assertIn('contact_email', response.context)
-            self.assertIsNone(response.context['contact_email'])
+            self.assertEqual(response.context['contact_email'], settings.CONTACT_EMAIL)
             with self.settings(CONTACT_EMAIL=example_email):
                 response = self.client.get(reverse('myhpom:dashboard'))
                 self.assertIn('contact_email', response.context)
@@ -32,8 +31,7 @@ class ContactEmailProcessorTestCase(TestCase):
     def test_email_is_availab_on_static_views(self):
         for pattern, name, template in static_views:
             response = self.client.get(reverse('myhpom:%s' % name))
-            self.assertIn('contact_email', response.context)
-            self.assertIsNone(response.context['contact_email'])
+            self.assertEqual(response.context['contact_email'], settings.CONTACT_EMAIL)
             for example_email in ('foo@example.com', 'bar@example.com', ):
                 with self.settings(CONTACT_EMAIL=example_email):
                     response = self.client.get(reverse('myhpom:%s' % name))
