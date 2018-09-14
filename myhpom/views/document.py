cb7fa99ea252917b735ccd4f05204d2896f89cae
Sean Harrison <sharrison@caktusgroup.com>
2018-09-14 15:13:34 -0500

    [mh-251] "I'd suggest splitting it [this big if block] out into the conceptual checks (ip, expiration, etc) so that its clearer what conditions this view is enforcing"

--- myhpom/views/document.py
+++ myhpom/views/document.py
@@ -15,16 +15,22 @@ def document_url(request, key, filename):
     * filename: included in the URL to provide a sensible filename and to verify the URL
     """
     # protect on client_ip, doc_url match, expiration, and filename
-    client_ip, _ = get_client_ip(request)
+    # -- the DocumentUrl has to exist and match
     doc_url = DocumentUrl.objects.filter(key=key).first()
     if (
-        not client_ip
-        or not doc_url
-        or (doc_url.expiration is not None and doc_url.expiration < now())
-        or not doc_url.authorized_client_ip(client_ip)
+        not doc_url
         or not doc_url.advancedirective.document
         or filename != doc_url.advancedirective.filename
     ):
+        raise Http404()
+
+    # -- the client_ip should exist and be authorized
+    client_ip, _ = get_client_ip(request)
+    if not client_ip or not doc_url.authorized_client_ip(client_ip):
+        raise Http404()
+
+    # -- the document url should not be expired
+    if doc_url.expiration and doc_url.expiration < now():
         raise Http404()
 
     # If we've gotten this far, the doc_url is valid, so send the document
