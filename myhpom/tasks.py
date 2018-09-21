a596936f8732880ef15871617505f18e609474d7
Sean Harrison <sharrison@caktusgroup.com>
2018-09-21 15:26:08 -0500

    [mh-254] new tasks to Cancel and Update a CloudFactoryRun via the CF API

--- myhpom/tasks.py
+++ myhpom/tasks.py
@@ -7,7 +7,8 @@ from django.template import Context
 from celery import shared_task
 from celery.task import Task
 from celery.signals import task_failure
-from myhpom.models import AdvanceDirective, DocumentUrl, CloudFactoryRun, CloudFactoryUnit
+from myhpom.models.document import AdvanceDirective, DocumentUrl
+from myhpom.models.cloudfactory import CloudFactoryRun, CloudFactoryUnit
 
 
 @shared_task
@@ -95,6 +96,68 @@ class CloudFactorySubmitAdvanceDirectiveRun(Task):
         # only non-blank values are submitted, to prevent present but useless values
         unit_input = {k: v for k, v in data.items() if bool(v) is True}
         return unit_input
+
+
+@shared_task
+class CloudFactoryCancelAdvanceDirectiveRun(Task):
+    def run(self, cf_run_id):
+        """Find any CloudFactory runs for the given document_url and cancel them.
+        (The AdvanceDirective and DocumentUrl objects have probably already been deleted, 
+        so we pass the document_url as a string so as to avoid needing to refer to those models.)
+        ## Use Cases:
+        * when the user deletes their AdvanceDirective, this task is triggered automatically.
+        ## Parameters:
+        * document_url = a string representing a DocumentUrl.url
+        ## Process: For each matching run,
+        * If it is in progress (status='Processing'), cancel it at CloudFactory.
+            * 202 = CloudFactory accepted the request
+            * 405 = already processed / aborted at CloudFactory
+        * Get the run info from CloudFactory and update our models.
+            * 200 = Ok
+        * Any response status apart from those given should raise an exception / send error email,
+            because it means we have a bug / false assumption somewhere in our system.
+        """
+        cf_run = CloudFactoryRun.objects.get(id=cf_run_id)
+        if cf_run.status == 'Processing':
+            # abort the run
+            response = self.post_abort(cf_run.run_id)
+            if response.status_code not in [202, 405]:
+                response_json = json.dumps(response.json(), indent=2)
+                raise ValueError(
+                    """URL: %s\nResponse status: %d\nResponse Data: %s"""
+                    % (response.url, response.status_code, response_json)
+                )
+
+            # update the CloudFactoryRun object, needs another API call.
+            CloudFactoryUpdateAdvanceDirectiveRun(cf_run.id)  # no delay here!
+
+        return cf_run.data
+
+    def post_abort(self, run_id):
+        abort_url = "%s/runs/%s/abort" % (settings.CLOUDFACTORY_API_URL, run_id)
+        return requests.post(abort_url)
+
+
+@shared_task
+class CloudFactoryUpdateAdvanceDirectiveRun(Task):
+    """separate task for updating the CloudFactoryRun object from the CF API."""
+
+    def run(self, cf_run_id):
+        cf_run = CloudFactoryRun.objects.get(id=cf_run_id)
+        response = self.get_run(cf_run.run_id)
+        response_data = response.json()  # confusingly named, response.json() is a dict
+        response_json = json.dumps(response_data, indent=2)  # so this is a string
+        if response.status_code != 200:
+            raise ValueError(
+                """URL: %s\nResponse status: %d\nResponse Data: %s"""
+                % (response.url, response.status_code, response_json)
+            )
+        cf_run.save_cloudfactory_response(response_data)  # takes the dict, not the string
+        return cf_run.data
+
+    def get_run(self, run_id):
+        run_url = "%s/runs/%s" % (settings.CLOUDFACTORY_API_URL, run_id)
+        return requests.get(run_url)
 
 
 @shared_task
