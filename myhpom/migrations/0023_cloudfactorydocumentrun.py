530bd91d8b806b9e6ff682d63abda0274a2fcd01
Sean Harrison <sharrison@caktusgroup.com>
2018-09-25 13:00:28 -0500

    [mh-252] new status choices require updating the migration.

--- myhpom/migrations/0023_cloudfactorydocumentrun.py
+++ myhpom/migrations/0023_cloudfactorydocumentrun.py
@@ -19,7 +19,7 @@ class Migration(migrations.Migration):
                 ('document_host', models.CharField(default=b'', help_text=b'The document host to which this run is connected.', max_length=64, blank=True)),
                 ('inserted_at', models.DateTimeField(help_text=b'When the run instance was inserted into our system.', auto_now_add=True)),
                 ('run_id', models.CharField(null=True, default=None, max_length=32, blank=True, help_text=b'The id of this production run at CloudFactory.', unique=True)),
-                ('status', models.CharField(default=b'NEW', help_text=b'The status of the run.', max_length=16, choices=[(b'NEW', b'NEW'), (b'DELETED', b'DELETED'), (b'Processing', b'Processing'), (b'Processed', b'Processed')])),
+                ('status', models.CharField(default=b'NEW', help_text=b'The status of the run.', max_length=16, choices=[(b'NEW', b'NEW'), (b'DELETED', b'DELETED'), (b'TIMEOUT', b'TIMEOUT'), (b'NOTFOUND', b'NOTFOUND'), (b'UNPROCESSABLE', b'UNPROCESSABLE'), (b'Processing', b'Processing'), (b'Processed', b'Processed'), (b'Aborted', b'Aborted')])),
                 ('created_at', models.DateTimeField(help_text=b'When the run was created at CloudFactory.', null=True, blank=True)),
                 ('processed_at', models.DateTimeField(help_text=b'When run processing was finished at CloudFactory.', null=True, blank=True)),
                 ('post_data', models.TextField(default=b'', help_text=b'The raw data that was submitted to CloudFactory when this run was created', blank=True)),
